3aff9fe61b4f65b37d9aeea956fb01ba7333e25e
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-06-09 13:35:45 -0600

    initial settings.py

---
+++ wmlci/settings.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from esupy.processed_data_mgmt import Paths, mkdir_if_missing
+
+
+MODULEPATH = Path(__file__).resolve().parent
+
+datapath = MODULEPATH / 'data'
+# warmdatapath = datapath / 'warm_v16_openlca_database_Mar2022'
+
+# "Paths()" are a class defined in esupy
+paths = Paths()
+paths.local_path = paths.local_path / 'wmlci'
+outputpath = paths.local_path
