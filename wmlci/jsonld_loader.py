fabc4426feba36aacd295d809561f54dd349749f
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2026-07-01 14:06:22 -0600

    rename common to jsonld loader

--- wmlci/jsonld_loader.py
+++ wmlci/jsonld_loader.py
@@ -17,32 +17,6 @@ from esupy.util import make_uuid
 from esupy.processed_data_mgmt import download_from_remote, Paths, mkdir_if_missing
 
 
-def assign_uuid():
-    """
-    Assign new UUID based on name/location/category fields. Uses EPA's useupy package for assignment
-    :return:
-    """
-
-    # todo: edit for bw code - pulled example code from fedelemflowlist repo
-
-    # # option 1 - class
-    # # set the UUID or generate it from the attributes
-    # if self.uid is None:
-    #     flow_ref.id = make_uuid("Flow", self.category, self.name)
-    # else:
-    #     flow_ref.id = self.uid
-    #
-    # # option 2 - for loop
-    # # Loop through flows generating UUID for each
-    # flowids = []
-    # log.info('Generating unique UUIDs for each flow...')
-    # for index, row in flows.iterrows():
-    #     flowid = make_uuid(row['Flowable'], row['Context'], row['Unit'])
-    #     flowids.append(flowid)
-    # flows['Flow UUID'] = flowids
-
-    # return
-
 def download_source_data_from_remote(fname):
     """
     Download source data stored from USEPA's data commons to local directory
